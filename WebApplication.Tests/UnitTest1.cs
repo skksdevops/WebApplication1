ff01642c61673136017fb62a586d68788a5fcc46
Saurabh Kamble <saurabhdevops@outlook.com>
2021-09-14 12:09:09 +0530

    saved

---
+++ WebApplication.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace WebApplication.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
